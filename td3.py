a8b89209f400d048f380c313156165aebc2e58f6
Jaehyun-Jeong <jj.for.jaehyun@gmail.com>
2026-02-02 21:16:15 +0900

    fixed overall algorithm for episodic loop, fixed replay buffer class

--- td3.py
+++ td3.py
@@ -1,19 +1,20 @@
-from types import SimpleNamespace
 import gymnasium as gym, torch as T, torch.nn as nn, torch.nn.functional as F
+from types import SimpleNamespace
 
-q_lr, pi_lr, gamma, tau, pi_noise, noise_clip, pi_freq, exploration_noise = 3e-4, 3e-4, 0.98, 0.005, 0.1, 0.5, 2, 0.1
-seed, learning_starts, total_timesteps, buffer_limit, batch_size, action_scale = 0, 25e3, 1_000_000, 1_000_000, 256, T.tensor(2, dtype=T.float32)
+q_lr, pi_lr, gamma, tau, pi_noise, noise_clip, pi_freq, exploration_noise = 1e-3, 1e-3, 0.98, 0.005, 0.1, 0.5, 1, 0.1
+learning_starts, total_timesteps, buffer_limit, batch_size, action_scale = 1_000, 1_000_000, 200_000, 256, T.tensor(2, dtype=T.float32)
 
 class ReplayBuffer:
     def __init__(self):
-        self.s, self.a, self.r, self.sp, self.d = [T.empty(0, i) for i in [3, 1, 1, 3, 1]]  # state, action, reward, s prime, done
+        self.s, self.a, self.r, self.sp, self.d = [T.zeros(buffer_limit, i) for i in [3, 1, 1, 3, 1]]  # state, action, reward, s prime, done
+        self.buffer_ptr, self.buffer_size = 0, 0
 
     def push(self, transition):
         s, a, r, sp, d = transition
-        self.s, self.a, self.r, self.sp, self.d = [
-            T.cat((T.Tensor(new).unsqueeze(0), buffer), 0)[:buffer_limit]
-            for new, buffer in zip([s, a, [r], sp, [d]], [self.s, self.a, self.r, self.sp, self.d])
-        ]
+        for new, buffer in zip([s, a, [r], sp, [d]], [self.s, self.a, self.r, self.sp, self.d]):
+            buffer[self.buffer_ptr].copy_(T.as_tensor(new))
+        self.buffer_ptr = (self.buffer_ptr + 1) % buffer_limit
+        self.buffer_size = min(self.buffer_size + 1, buffer_limit)
 
     def sample(self, n):
         idx = T.randperm(len(self))[:n]
@@ -24,7 +25,7 @@ class ReplayBuffer:
 class QNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc = nn.Sequential(nn.Linear(4, 256), nn.ReLU(), nn.Linear(256, 1))
+        self.fc = nn.Sequential(nn.Linear(4, 128), nn.ReLU(), nn.Linear(128, 32), nn.ReLU(), nn.Linear(32, 1))
 
     def forward(self, x, a): return self.fc(T.cat([x, a], 1))
 
@@ -33,68 +34,67 @@ class PolicyNet(nn.Module):
         super().__init__()
         self.fc_mu = nn.Sequential(nn.Linear(3, 256), nn.ReLU(), nn.Linear(256, 1))
 
-    def forward(self, x): return T.tanh(self.fc_mu(x))
+    def forward(self, x): return T.tanh(self.fc_mu(x)) * action_scale
+
+def calc_target(pi_t, q1_t, q2_t, mb):
+    with T.no_grad():
+        clipped_noise = (T.randn_like(mb.a) * pi_noise).clamp(
+            -noise_clip, noise_clip
+        ) * action_scale
+        next_state_action = (pi_t(mb.sp) + clipped_noise).clamp(
+            env.action_space.low[0], env.action_space.high[0]
+        )
+        q1_next_target, q2_next_target = [q_t(mb.sp, next_state_action) for q_t in [q1_t, q2_t]]
+        min_q_next_target = T.min(q1_next_target, q2_next_target)
+        return (mb.r + gamma * mb.done * min_q_next_target).view(-1)
 
 if __name__ == "__main__":
-    env = gym.make('Pendulum-v1')
+    env, memory = gym.make('Pendulum-v1'), ReplayBuffer()
+    q1, q2, q1_t, q2_t, pi, pi_t = QNet(), QNet(), QNet(), QNet(), PolicyNet(), PolicyNet()
+    pi_t.load_state_dict(pi.state_dict())
+    pi_optimizer = T.optim.Adam(list(pi.parameters()), lr=pi_lr)
+    [qt.load_state_dict(q.state_dict()) for qt, q in [(q1_t, q1), (q2_t, q2)]]
+    q_optimizer = T.optim.Adam(list(q1.parameters()) + list(q2.parameters()), lr=q_lr)
 
-    actor, actor_target = PolicyNet(), PolicyNet()
-    actor_target.load_state_dict(actor.state_dict())
-    actor_optimizer = T.optim.Adam(list(actor.parameters()), lr=pi_lr)
-    qf1, qf1_target, qf2, qf2_target = [QNet() for _ in range(4)]
-    qf1_target.load_state_dict(qf1.state_dict())
-    qf2_target.load_state_dict(qf2.state_dict())
-    q_optimizer = T.optim.Adam(list(qf1.parameters()) + list(qf2.parameters()), lr=q_lr)
-    rb = ReplayBuffer()
-
-    obs, _ = env.reset(seed=seed)
+    s, _ = env.reset()
     score = 0.
-    for global_step in range(total_timesteps):
-        if global_step < learning_starts:
-            action = env.action_space.sample()
-        else:
-            with T.no_grad():
-                action = actor(T.Tensor(obs))
-                action += T.normal(0, action_scale * exploration_noise)  # Smoothing q funciton
-                action = action.cpu().numpy().clip(env.action_space.low, env.action_space.high)
+    for n_epi in range(10000):
+        for num_step in range(200):
+            if len(memory) < learning_starts:
+                a = env.action_space.sample()
+            else:
+                with T.no_grad():
+                    a = pi(T.Tensor(s))
+                    a += T.normal(0, action_scale * exploration_noise)  # Smoothing q funciton
+                    a = a.cpu().numpy().clip(env.action_space.low, env.action_space.high)
+            sp, r, done, _, _ = env.step(a)
+            memory.push((s, a, r/10.0, sp, 0. if done else 1.))
+            score += r
+            s = sp
 
-        next_obs, reward, done, _, _ = env.step(action)
-        real_next_obs = next_obs.copy()
-        score += reward
-        rb.push((real_next_obs, action, reward, obs, done))
-        obs = next_obs
+            if len(memory) > learning_starts:
+                mb = memory.sample(batch_size)
+                next_q_value = calc_target(pi_t, q1_t, q2_t, mb)
+                q1_a_value, q2_a_value = [q(mb.s, mb.a).view(-1) for q in [q1, q2]]
+                q_loss = F.mse_loss(q1_a_value, next_q_value) + F.mse_loss(q2_a_value, next_q_value)
 
-        if global_step > learning_starts:
-            data = rb.sample(batch_size)
-            with T.no_grad():
-                clipped_noise = (T.randn_like(data.a) * pi_noise).clamp(
-                    -noise_clip, noise_clip
-                ) * action_scale
-                next_state_action = (actor_target(data.s) + clipped_noise).clamp(
-                    env.action_space.low[0], env.action_space.high[0]
-                )
-                qf1_next_target, qf2_next_target = [q_target(data.s, next_state_action) for q_target in [qf1_target, qf2_target]]
-                min_qf_next_target = T.min(qf1_next_target, qf2_next_target)
-                next_q_value = (data.r + gamma * (1 - data.done) * min_qf_next_target).view(-1)
-            qf1_a_value, qf2_a_value = [qf(data.sp, data.a).view(-1) for qf in [qf1, qf2]]
-            qf_loss = F.mse_loss(qf1_a_value, next_q_value) + F.mse_loss(qf2_a_value, next_q_value)
+                q_optimizer.zero_grad()
+                q_loss.backward()
+                q_optimizer.step()
 
-            q_optimizer.zero_grad()
-            qf_loss.backward()
-            q_optimizer.step()
+                if (num_step+1) % pi_freq == 0:  # Delayed Policy Update
+                    pi_loss = -q1(mb.s, pi(mb.s)).mean()
+                    pi_optimizer.zero_grad()
+                    pi_loss.backward()
+                    pi_optimizer.step()
 
-            if global_step % pi_freq == 0:  # Delayed Policy Update
-                actor_loss = -qf1(data.sp, actor(data.sp)).mean()
-                actor_optimizer.zero_grad()
-                actor_loss.backward()
-                actor_optimizer.step()
+                    for nn, nn_t in zip([pi, q1, q2], [pi_t, q1_t, q2_t]):  # EMA
+                        for param, param_t in zip(nn.parameters(), nn_t.parameters()):
+                            param_t.data.copy_(tau * param.data + (1 - tau) * param_t.data)
+            if done: break
 
-                # Polyak averaging
-                for nn, nn_target in zip([actor, qf1, qf2], [actor_target, qf1_target, qf2_target]):
-                    for param, param_target in zip(nn.parameters(), nn_target.parameters()):
-                        param_target.data.copy_(tau * param.data + (1 - tau) * param_target.data)
+        if n_epi % 20 == 0 and n_epi != 0:
+            print(f"Episode: {n_epi}, Avg Score: {score/20:.1f}")
+            score = 0.0
 
-            if global_step % 100 == 0:
-                print(f"# of step:{global_step}, avg score : {score/20:1f}")
-                score = 0.0
     env.close()
